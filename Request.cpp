b0f23b86fb783f6b5803521ed453e6fb813d3e3b
John <johnzionwong33@gmail.com>
2025-07-15 12:22:13 -0700

    implement request

--- Request.cpp
+++ Request.cpp
@@ -1,2 +1,10 @@
 #include "Request.h"
 
+#include <string>
+using std::string;
+
+Request::Request(const string& ipIN, const string& ipOUT, const int processTime) {
+    this->ipIN = ipIN;
+    this->ipOUT = ipOUT;
+    this->processTime = processTime;
+}
--- Request.h
+++ Request.h
@@ -1,6 +1,16 @@
 #ifndef REQUEST_H
 #define REQUEST_H
 
+#include <string>
+using std::string;
 
+struct Request {        
+    
+    string ipIN;
+    string ipOUT;
+    int processTime;
+
+    Request(const string& ipIN, const string& ipOUT, const int processTime);
+};
 
 #endif
