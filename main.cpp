f1eea6fa5ae2f6e92b83022ff3ba8faf511bc2bd
John <johnzionwong33@gmail.com>
2025-07-15 18:30:23 -0700

    main init

--- main.cpp
+++ main.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
 
+#include "Request.h"
+#include "WebServer.h"
+#include "LoadBalancer.h"
+
+using namespace std;
+
+int main() {
+    
+    // Get Input (# of web servers and time to run)
+    int numWebServers = 0, timeToRun = 0;
+    cout << "Number of Web Servers: ";
+    cin >> numWebServers;
+    cout << endl;
+    cout << "Number of Cycles to run for: ";
+    cin >> numWebServers;
+    cout << endl;
+
+    // generate full queue (numWebServers * 100)
+    
+
+}
