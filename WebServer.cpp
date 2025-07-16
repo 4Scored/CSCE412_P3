5be5e37897fc503ec200e9a244eed08986b6e6a8
John <johnzionwong33@gmail.com>
2025-07-15 21:06:27 -0700

    WebServer getReqToProcess and processReq implement

--- WebServer.cpp
+++ WebServer.cpp
@@ -1,19 +1,23 @@
 #include "WebServer.h"
 
-#include <chrono>
-
 WebServer::WebServer(const int id) {
     this->serverID = id; 
     this->processTime = 0;
     this->isFree = true;
 }
 
-void WebServer::processRequest(const Request& request) {
-
+void WebServer::getRequestToProcess(const Request& request) {
+    this->processTime = request.processTime;
+    this->isFree = false;
 }
 
-void WebServer::getRequestToProcess(const Request& request) {
-
+void WebServer::processRequest(const Request& request) {
+    if (!this->isFree) { // If not free, its taken a request
+        this->processTime--; // Decrement current request's process time
+        if (this->processTime <= 0) {
+            this->isFree = true; // Freed after process time is up
+        }
+    }
 }
 
 bool WebServer::checkAvailability() {
